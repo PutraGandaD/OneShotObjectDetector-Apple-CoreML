82b2fc3aeb5031d8bdf8bf57bb1c2718a0779a1a
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2026-05-20 20:38:49 +0700

    Initial Commit

---
+++ PoC_OneShotObjectDetector/PoC_OneShotObjectDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoC_OneShotObjectDetectorApp.swift
+//  PoC_OneShotObjectDetector
+//
+//  Created by Putra Ganda Dewata on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PoC_OneShotObjectDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
